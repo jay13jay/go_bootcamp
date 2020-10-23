9d4c1953ad0ff49c72f28bbb7df1b57bc76b58b4
Josh Hackney <josh.hackney13@gmail.com>
2020-10-23 17:54:54 -0400

    var2var excerdise

---
+++ assignment_exercises/var2var/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// UNCOMMENT THE CODE BELOW:
+
+	color := "green"
+	modifier := "dark "
+	color = modifier + color
+
+	// ADD YOUR CODE BELOW
+
+	// ?
+
+	// UNCOMMENT THE CODE BELOW TO PRINT THE VARIABLE
+
+	fmt.Println(color)
+}
