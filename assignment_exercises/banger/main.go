e4fa54f470614f8bab43a6bdfb0fcc8a00bfe8e4
Josh Hackney <josh.hackney13@gmail.com>
2020-10-24 19:38:28 -0400

    add banger exercise 85

---
+++ assignment_exercises/banger/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var word string
+
+func printbang(word string) {
+	banger := strings.Repeat("!", len(word))
+	fmt.Printf("%s%s\n", word, banger)
+	// fmt.Printf("%s%s%s\n", banger, word, banger)
+
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("no word provided!")
+		os.Exit(1)
+	}
+	word = os.Args[1]
+
+	printbang(word)
+
+}
