3784c576a7150d043c7c9f0b02d135badad49fdf
Josh Hackney <josh.hackney13@gmail.com>
2020-10-23 23:57:54 -0400

    added greeter exercise

---
+++ getinput/greeter/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+func main() {
+	// fmt.Printf("%#v\n", os.Args)
+	// fmt.Printf("%v\n\n", os.Args)
+
+	_, file := path.Split(os.Args[0])
+
+	fmt.Printf("Program\t\t: %v\n", file)
+	fmt.Println("Provided Args\t: ", len(os.Args)-1)
+
+	count := 1
+	if len(os.Args) >= 1 {
+		for i := 0; i < len(os.Args)-1; i++ {
+			fmt.Printf("Hello, %v!\n", os.Args[count])
+			count = count + 1
+		}
+	}
+
+}
