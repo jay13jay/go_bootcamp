0a799576afc3d2176add9eb170b6396636825bdc
Josh Hackney <josh.hackney13@gmail.com>
2020-10-23 22:21:24 -0400

    cleaned up the print statements to be less redundant, and pretty print the program name

--- getinput/osArgsbasics/main.go
+++ getinput/osArgsbasics/main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 )
 
 func main() {
-	fmt.Printf("%#v\n", os.Args)
-	fmt.Printf("%v\n\n", os.Args)
+	// fmt.Printf("%#v\n", os.Args)
+	// fmt.Printf("%v\n\n", os.Args)
 
-	fmt.Printf("Program:\t%v\n", os.Args[0])
-	fmt.Println("# of Arguments:\t", len(os.Args))
+	_, file := path.Split(os.Args[0])
+
+	fmt.Printf("Program\t\t: %v\n", file)
+	fmt.Println("Provided Args\t: ", len(os.Args)-1)
 
 	count := 1
 	if len(os.Args) >= 1 {
 		for i := 0; i < len(os.Args)-1; i++ {
-			fmt.Printf("Argument %v:\t%v\n", count, os.Args[count])
+			fmt.Printf("Argument %v\t: %v\n", count, os.Args[count])
 			count = count + 1
 		}
 	}
