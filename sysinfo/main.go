1b75e70eb07254e6d9650c973da02e3de3eb1a17
Josh Hackney <josh.hackney13@gmail.com>
2020-10-27 18:02:33 -0400

    strings practice for section 87

--- sysinfo/main.go
+++ sysinfo/main.go
@@ -5,7 +5,32 @@ import (
 	"runtime"
 )
 
+var numcpu = runtime.NumCPU()
+
 func main() {
-	var num_cpu = runtime.NumCPU()
-	fmt.Printf("Number of CPUs:\t %d\n", num_cpu)
+	printCPUuse()
+	printMemUsage()
+
+}
+
+func printCPUuse() {
+	fmt.Printf("\nNumber of CPUs:\t %d\n", numcpu)
+}
+
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	// m := runtime.ReadMemStats(&runtime.MemStats) // This doesn't work, not sure why yet, but I am way ahead of the lectures so will come back later
+
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n\n", m.NumGC)
+	fmt.Printf("unformated print:\n\t%v", m)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
